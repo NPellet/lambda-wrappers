5dc674a511533154010c8e512f7f258c6399014a
Norman Pellet <norman.pellet@lendis.de>
2022-12-06 12:00:20 +0100

    Export request

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -2,3 +2,4 @@ export { createEventBridgeHandler } from "./EventBridge/event";
 export { createApiGatewayHandler } from "./ApiGateway/api";
 
 export { Response } from "../util/apigateway/response";
+export { Request } from "../util/apigateway/request";
