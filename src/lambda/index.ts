22c0aa274390386385a582c6ef85f79af922287f
Norman Pellet <norman.pellet@lendis.de>
2022-12-07 08:54:18 +0100

    Add export HTTP Error

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -7,7 +7,7 @@ export {
   apiGatewayHandlerFactory,
 } from "./ApiGateway/api";
 
-export { Response } from "../util/apigateway/response";
+export { Response, HTTPError } from "../util/apigateway/response";
 export { Request } from "../util/apigateway/request";
 
 export { getAwsSecretDef } from "../lambda/utils/secrets_manager";
