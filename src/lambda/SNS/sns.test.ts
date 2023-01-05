ee260651b0dc5bffdc45dcf50c47f7ff361e9fc6
Norman Pellet <norman.pellet@gmail.com>
2023-01-05 08:21:57 +0100

    Add basic SNS test

--- src/lambda/SNS/sns.test.ts
+++ src/lambda/SNS/sns.test.ts
@@ -1,125 +1,127 @@
-import { LambdaContext, testSNSEvent, testSNSRecord } from "../../test_utils/utils";
-import { MessageType } from "../../util/types"
-import { createSNSHandler } from "./sns"
-import * as yup from "yup";
-
+import {
+  LambdaContext,
+  testSNSEvent,
+  testSNSRecord,
+} from '../../test_utils/utils';
+import { MessageType } from '../../util/types';
+import { createSNSHandler } from './sns';
+import * as yup from 'yup';
 
 jest.mock('../../util/exceptions', function () {
-    return {
-      recordException: jest.fn(),
-    };
+  return {
+    recordException: jest.fn(),
+  };
+});
+import { recordException } from '../../util/exceptions';
+
+jest.mock('./telemetry/Wrapper', function () {
+  const moduleContent = jest.requireActual('./telemetry/Wrapper');
+  return {
+    ...moduleContent,
+    wrapTelemetrySNS: jest.fn(moduleContent.wrapTelemetrySNS),
+  };
+});
+
+import { wrapTelemetrySNS } from './telemetry/Wrapper';
+
+describe('SNS: Telemetry', function () {
+  it('Calls OTEL wrapper following opentelemetry flag', async () => {
+    createSNSHandler(async (data) => {}, {
+      opentelemetry: true,
+      messageType: MessageType.String,
+    });
+
+    expect(wrapTelemetrySNS).toHaveBeenCalledTimes(1);
+    jest.clearAllMocks();
+
+    createSNSHandler(async (data) => {}, {
+      opentelemetry: false,
+      messageType: MessageType.String,
+    });
+
+    expect(wrapTelemetrySNS).not.toHaveBeenCalled();
   });
-  import { recordException } from '../../util/exceptions';
-  
+});
 
-jest.mock('./telemetry/Wrapper', function() {
+describe('SNS: Runtime config', function () {
+  it('General behaviour', async () => {
+    const handler = createSNSHandler(async () => {}, {
+      messageType: MessageType.String,
+    });
 
-    const moduleContent= jest.requireActual('./telemetry/Wrapper');
-    return {
-      ...moduleContent,
-      wrapTelemetrySNS: jest.fn( moduleContent.wrapTelemetrySNS )
-    }
-  })
+    await expect(
+      handler(testSNSEvent, LambdaContext, () => {})
+    ).resolves.toBeUndefined();
+  });
 
-  import { wrapTelemetrySNS } from "./telemetry/Wrapper";
-  
-describe("SNS: Telemetry", function() {
-
-    it("Calls OTEL wrapper following opentelemetry flag", async() => {
-
-        createSNSHandler( async ( data ) => {}, {
-            opentelemetry: true,
-            messageType: MessageType.String
-        })
-
-        expect( wrapTelemetrySNS ).toHaveBeenCalledTimes( 1 );
-        jest.clearAllMocks();
-
-        createSNSHandler( async ( data ) => {},{
-            opentelemetry: false,
-            messageType: MessageType.String
-        })
-
-        expect( wrapTelemetrySNS ).not.toHaveBeenCalled( );
-    })
-})
-
-describe("SNS: Runtime config", function() {
-
-
-    it("Validation failure silences the lambda", async() => {
-
-        const handler = createSNSHandler( async () => {}, {
-            "sources": {
-                "sns": {
-                    "silenceRecordOnValidationFail": true
-                }
-            },
-            messageType: MessageType.String,
-            yupSchemaInput: yup.object({
-                field: yup.number().required(),
-              }),
-        });
-
-        await expect( handler( testSNSEvent, LambdaContext, () => {} ) ).resolves.toBeUndefined()
-
-
-        const handler2 = createSNSHandler( async () => {}, {
-            "sources": {
-                "sns": {
-                    "silenceRecordOnValidationFail": false
-                }
-            },
-            messageType: MessageType.String,
-            yupSchemaInput: yup.object({
-                field: yup.number().required(),
-              }),
-        })
-        
-        await expect( handler2( testSNSEvent, LambdaContext, () => {} ) ).rejects.toBeDefined()
-    })
-
-    
-  it("Validation failure records exception", async () => {
-
-    const handler = createSNSHandler(
-      async (request) => {},
-      {
-        sources: {
-          sns: {
-            recordExceptionOnValidationFail: true,
-            silenceRecordOnValidationFail: true
-          }
+  it('Validation failure silences the lambda', async () => {
+    const handler = createSNSHandler(async () => {}, {
+      sources: {
+        sns: {
+          silenceRecordOnValidationFail: true,
         },
-        yupSchemaInput: yup.object({
-          field: yup.number().required(),
-        }),
-        messageType: MessageType.String,
-      }
-    );
-    await handler( testSNSEvent, LambdaContext, () => {} );
-    expect( recordException ).toHaveBeenCalled()
+      },
+      messageType: MessageType.String,
+      yupSchemaInput: yup.object({
+        field: yup.number().required(),
+      }),
+    });
+
+    await expect(
+      handler(testSNSEvent, LambdaContext, () => {})
+    ).resolves.toBeUndefined();
+
+    const handler2 = createSNSHandler(async () => {}, {
+      sources: {
+        sns: {
+          silenceRecordOnValidationFail: false,
+        },
+      },
+      messageType: MessageType.String,
+      yupSchemaInput: yup.object({
+        field: yup.number().required(),
+      }),
+    });
+
+    await expect(
+      handler2(testSNSEvent, LambdaContext, () => {})
+    ).rejects.toBeDefined();
+  });
+
+  it('Validation failure records exception', async () => {
+    const handler = createSNSHandler(async (request) => {}, {
+      sources: {
+        sns: {
+          recordExceptionOnValidationFail: true,
+          silenceRecordOnValidationFail: true,
+        },
+      },
+      yupSchemaInput: yup.object({
+        field: yup.number().required(),
+      }),
+      messageType: MessageType.String,
+    });
+    await handler(testSNSEvent, LambdaContext, () => {});
+    expect(recordException).toHaveBeenCalled();
 
     jest.clearAllMocks();
 
-    const handler2 = createSNSHandler(
-      async (request) => {},
-      {
-        sources: {
-          sns: {
-            recordExceptionOnValidationFail: false,
-            silenceRecordOnValidationFail: true
-          }
+    const handler2 = createSNSHandler(async (request) => {}, {
+      sources: {
+        sns: {
+          recordExceptionOnValidationFail: false,
+          silenceRecordOnValidationFail: true,
         },
+      },
 
-        yupSchemaInput: yup.object({
-          field: yup.number().required(),
-        }),
-        messageType: MessageType.String,      
+      yupSchemaInput: yup.object({
+        field: yup.number().required(),
+      }),
+      messageType: MessageType.String,
     });
-    
-    await handler2( testSNSEvent, LambdaContext, () => {} );
 
-    expect( recordException ).not.toHaveBeenCalled()
-  })
-})
+    await handler2(testSNSEvent, LambdaContext, () => {});
+
+    expect(recordException).not.toHaveBeenCalled();
+  });
+});
