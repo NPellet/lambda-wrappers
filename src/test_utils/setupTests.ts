61f3719f647e87a8af323f8627188a7e370d75a3
Norman Pellet <norman.pellet@gmail.com>
2023-01-11 15:49:04 +0100

    Remove implementation specific opentelemetry reset

--- src/test_utils/setupTests.ts
+++ src/test_utils/setupTests.ts
@@ -1,10 +1,10 @@
 import { traceContextEnvironmentKey } from '../lambda/utils/telemetry';
 import { memoryExporter, setupOtel } from './utils';
-const GLOBAL_OPENTELEMETRY_API_KEY = Symbol.for(`opentelemetry.js.api.1`);
+import api from '@opentelemetry/api';
 
 beforeEach(() => {
   // Allow reset of the metrics
-  globalThis[GLOBAL_OPENTELEMETRY_API_KEY]['metrics'] = undefined; // Allow reset of metrics
+  api.metrics.disable();
   setupOtel();
 });
 
